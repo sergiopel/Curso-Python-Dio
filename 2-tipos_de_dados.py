a3ccd818e5c9d97002c303c21fb677fc3c07b38e
Sergio Pelegrini <sergio.pelegrini@scorpios.com.br>
2022-10-25 13:00:59 -0300

    Primeiro commit

---
+++ 2-tipos_de_dados.py
@@ -0,0 +1,13 @@
+print(11 + 10 + 1000)
+print(1.5 + 1 + 8.5)
+print(True)
+print(False)
+print("Python")
+
+#int()
+#float()
+#str()
+#bool()
+
+# digitando python no terminal, é possível executar cálculos sem a 
+# necessidade de usar o print
---
+++ 3-modo_interativo.py
@@ -0,0 +1,15 @@
+# abrir terminal: clicar no menu Terminal > New terminal
+# digitar no terminal: python
+## pode-se fazer contas
+## pra sair do terminal: exit()
+
+# digitar python -i nome_do_programa.py
+## depois de executar, abre o modo interativo (>>>)
+
+# Função dir:
+## dir(), mostra o escopo local
+## import math
+## dir(), tem o acrescimo do math no escopo local
+
+## dir(100) ou dir(1),
+## lista todos os métodos que dá pra utilizar como parâmetro do tipo int (100)
