dbd18ae678e70ad383acddcaa80d5f9bf1bcf611
Sergio Pelegrini <sergio.pelegrini@scorpios.com.br>
2022-10-31 10:05:11 -0300

    atualização 4 e 5

---
+++ 4-variaveis_contantes.py
@@ -0,0 +1,24 @@
+from string import octdigits
+
+
+# No Python não existe definição de constante, o que existe é uma convenção para 
+# usar o nome das constantes em maiúsculo. Como não é uma regra, então dá pra alterar
+# o valor de uma constante
+
+# Boas práticas
+# o padrão dos nomes é snake case, exemplos:
+## taxa_juros
+## limite_diario
+
+nome = 'Sergio'
+idade = 50
+
+nome, idade = "Sophia", 14,
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS"]
+
+print(BRAZILIAN_STATES)
---
+++ 5-conversao_tipos.py
@@ -0,0 +1,31 @@
+# converter float para int, neste exemplo retorna 1
+print(int(1.95))
+
+# converter string para inteiro
+print(int("10"))
+
+# converter string para float
+print(float("10.12"))
+
+# converter int para float
+print(float(100))
+
+# converter número para string
+print(str(10.10))
+
+# converter número para string, para comprovar que virou string fazemos o seguinte:
+print(type(str(10.10)))
+
+# mais exemplos para verificar o tipo da string:
+valor = 10
+valor_str = str(valor)
+print(type(valor))
+print(type(valor_str))
+
+# divisão retornando float (utiliza-se uma barra de divisão: '/')
+print(100 / 2)
+
+# divisão retornando inteiro (utiliza-se duas barras de divisão: '//')
+print(100 // 2)
+
+
