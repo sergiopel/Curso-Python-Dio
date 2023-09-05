560c12e028ed48f98b2bf8f4f42ef0e1808cc50b
Sergio Pelegrini <serpel@gmail.com>
2023-09-05 16:48:10 -0300

    sintaxe python - introdução - 05/09/23

--- string_1.py
+++ string_1.py
@@ -10,3 +10,24 @@ print(nome.title())
 
 
 texto = "  Olá mundo!     "
+print(texto + ".")
+# eliminar todos os espaços em branco
+print(texto.strip() + ".")
+# eliminar espaços à direta
+print(texto.rstrip() + ".")
+# eliminar espaços à esquerda
+print(texto.lstrip() + ".")
+
+# junções e centralização
+menu = "Python"
+print("####" + menu + "####")
+# a instrução acima pode ser substituí por:
+print(menu.center(14, "#"))
+
+# colocando um traço entre as letras da string:
+print("-".join(menu))
+
+# se fosse fazer com for, teria mais código, assim:
+for letra in menu:
+    print(letra, end="-")
+print()
